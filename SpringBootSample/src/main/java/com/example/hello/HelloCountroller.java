25446277ad0306c21b81890e7703a3c2370c57f0
yukishi24 <yukishi24github@gmail.com>
2023-01-16 21:26:48 +0900

    add: postReqest files

--- SpringBootSample/src/main/java/com/example/hello/HelloCountroller.java
+++ SpringBootSample/src/main/java/com/example/hello/HelloCountroller.java
@@ -1,7 +1,10 @@
 package com.example.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloCountroller {
@@ -9,5 +12,13 @@ public class HelloCountroller {
   public String getHello() {
     // helloに遷移
     return "hello";
+  }
+  @PostMapping("/hello")
+  public String postReqest(@RequestParam("text1")String str,Model model) {
+    //画面から受け取った文字列をmodelに登録
+    model.addAttribute("sample",str);
+    
+    //response.htmlに遷移
+    return "hello/response";
   }
 }
